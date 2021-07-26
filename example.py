16923f848fa6e9d77cdf5ccaeedcfb3d175f184b
Adi Cakra <adi.budi.cakra@protonmail.com>
2021-07-26 08:29:36 +0000

    remove secrets!

--- example.py
+++ example.py
@@ -4,10 +4,10 @@ from tweepy.streaming import StreamListener
 import pprint
 import json
 
-consumer_key = 'AuqelaIg0YLkWC4vToUV8bUMh'
-consumer_secret = 'SECNwsCczYbJlIAnxUyfd0ytDDCq2o4Fsr1ZGTMLdCskCxYwPn'
-access_token = '147892104-88JainiSiKVksISvRRBvwucpASZsfHKycnwEKWXj'
-access_secret = 'nGcAuRx6YAg405JSb6sV1SjkydONgg8H97IGAygvFvt1G'
+consumer_key = 'CONSUMER_KEY'
+consumer_secret = 'CONSUMER_SECRET'
+access_token = 'ACCESS_TOKEN'
+access_secret = 'ACCESS_SECRET'
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
